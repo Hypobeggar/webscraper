704b9f6583c060e8d502f2c54f089603cd810cc1
Zach Watson <89600337+Hypobeggar@users.noreply.github.com>
2024-04-15 20:12:51 -0500

    Update for Wikipedia ToC

--- wikiscraper/wikiscraper/spiders/wiki_spider.py
+++ wikiscraper/wikiscraper/spiders/wiki_spider.py
@@ -3,25 +3,26 @@ from pathlib import Path
 
 class WikiSpider(scrapy.Spider):
     name = "wiki"
+
     def start_requests(self):
         urls = [
-        "https://quotes.toscrape.com/" # inital seed
-        # https://en.wikipedia.org/wiki/Main_Page
-        # "https://en.wikipedia.org/wiki/Elmvale_Acres_Shopping_Centre"
+            "https://en.m.wikipedia.org/wiki/Sadie_Houck"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        page = response.url.split("/")[-1]
         filename = f"Wiki-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
-        title= response.css("span::text").extract()# "span::mw-page-title-main"
-        yield {"QUOTES ARE": title }
 
-        link= response.css("li.next a::attr(href)").get()
+        # Extract table of contents
+        toc = response.xpath('//div[@id="toc"]') # AI agent ChatGPT
 
-        if link is not None:
-            yield response.follow( link, callback= self.parse)
+        if toc:
+            yield {"table_of_contents": toc.extract()}
+
+        # Follow links in the TOC if any
+        for link in response.css("#toc a::attr(href)").getall():
+            yield response.follow(link, callback=self.parse)
